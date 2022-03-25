732b25e876998e477ed9fc91690d66e0ce425d15
Lucab1705 <lucabianchi1705@gmail.com>
2022-03-25 09:21:58 +0100

    something started working now

--- src/js/gethorny.js
+++ src/js/gethorny.js
@@ -23,41 +23,45 @@ const getData = async (nsfw) => {
     return data
 }
 
+const buildChartData = (data) => {
+    let datasets = []
+    for (const [key, value] of Object.entries(data)){
+        for (const [k, val] of Object.entries(value)){
+            datasets.push(
+                {
+                    label : key,
+                    data : [key, val],
+                    stack : 'Stack '+(k == "nsfw" ? 1 : 0) + Object.keys(data).findIndex((elem) => elem == key),
+                }
+            )
+        }
+    }
+    return {
+        labels: Object.keys(data),
+        datasets: datasets
+    }
+}
+
 const plotData = (graphData) => {
     const ctx = document.getElementById('chart').getContext('2d');
+    const data = buildChartData(graphData)
+
+    console.log(data)
     const myChart = new Chart(ctx, {
         type: 'bar',
-        data: {
-            labels: Object.keys(graphData),
-            datasets: [{
-                label: '# of Subs',
-                data: graphData[Object.keys(graphData)],
-                backgroundColor: [
-                    'rgba(255, 99, 132, 0.2)',
-                    'rgba(54, 162, 235, 0.2)',
-                    'rgba(255, 206, 86, 0.2)',
-                    'rgba(75, 192, 192, 0.2)',
-                    'rgba(153, 102, 255, 0.2)',
-                    'rgba(255, 159, 64, 0.2)'
-                ],
-                borderColor: [
-                    'rgba(255, 99, 132, 1)',
-                    'rgba(54, 162, 235, 1)',
-                    'rgba(255, 206, 86, 1)',
-                    'rgba(75, 192, 192, 1)',
-                    'rgba(153, 102, 255, 1)',
-                    'rgba(255, 159, 64, 1)'
-                ],
-                borderWidth: 1
-            }]
-        },
+        data: data,
         options: {
-            scales: {
-                y: {
-                    beginAtZero: true
-                }
+            plugins: {
+              title: {
+                display: true,
+                text: 'Chart.js Bar Chart - Stacked'
+              },
+            },
+            responsive: true,
+            interaction: {
+              intersect: false,
             }
-        }
+          }
     });
 }
 
